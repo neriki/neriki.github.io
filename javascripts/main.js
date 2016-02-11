0c894f5dae5fc276ae68d184091ea180f4e7a934
Neriki <neriki@free.fr>
2016-02-11 14:04:50 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,32 @@
-console.log('This would be the main JS file.');
+var txt = "NERIKI"
+var ch = {
+  "N": "|\\|",
+  "E": "3",
+  "R": "|?",
+  "I": "1",
+  "K": "|&lt;"
+};
+
+function changeText() {
+  var spantxt = document.getElementById("neriki");
+  //var txt = spantxt.innerHTML;
+	var ncase = Math.round(Math.random() * txt.length) - 1;
+  
+  
+
+  var nchar = Math.round(Math.random() * txt.length) - 1;
+  
+  if (nchar<0)nchar=0;
+
+	document.getElementById("debug").innerHTML=nchar;
+
+  var txt2 = txt.substring(0, nchar ) + ch[txt.substring(nchar, nchar + 1)] + txt.substring(nchar + 1, txt.length);
+
+  //txt += " pouet";
+
+  spantxt.innerHTML = txt2 + "    " + nchar;
+}
+
+
+setInterval(changeText, 5000);
+
